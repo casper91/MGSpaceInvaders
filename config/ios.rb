3c270c53f061edafabcb8a7b1b51fec467f7d7d1
Casper Christensen <casperc91@gmail.com>
2017-08-20 14:02:18 +0200

    Adds resources and provision

--- config/ios.rb
+++ config/ios.rb
@@ -10,6 +10,15 @@ Motion::Project::App.setup do |app|
   # Use `rake ios:config' to see complete project settings.
   app.name = 'MGSpaceInvaders'
 
+  app.icons = ['large', 'medium', 'small']
+
+  app.identifier = 'dk.ccapps.cch'
+
+  app.development do
+    app.provisioning_profile = 'provision/Development.mobileprovision'
+    app.codesign_certificate = 'iPhone Developer: Casper Christensen'
+  end
+
   # Set portrait mode only
   app.info_plist['UISupportedInterfaceOrientations'] = ['UIInterfaceOrientationPortrait']
 end
