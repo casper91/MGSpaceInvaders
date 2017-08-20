853d9ab28720e7fd6bbe7f14234b95e9c251c776
Casper Christensen <casperc91@gmail.com>
2017-08-20 14:12:28 +0200

    Added simple touch recognition

--- app/game_scene.rb
+++ app/game_scene.rb
@@ -1,9 +1,18 @@
 module MG
   class GameScene < Scene
+
     def initialize
-      label = Text.new("Hello World", "Arial", 96)
-      label.anchor_point = [0, 0]
-      add label
+      on_touch_begin { |event| @swipe_gesture = false ; true }
+      on_touch_move { |event| @swipe_gesture = true }
+      on_touch_end { |event| touch_handler(event) }
+    end
+
+    def touch_handler(event)
+      if @swipe_gesture
+        puts "Swipe detected!"
+      else
+        puts "Tap!"
+      end
     end
   end
 end
